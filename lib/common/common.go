3ea155713c0237a9ca2a0711b4661064e8363b6b
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-07-12 09:03:20 -0400

    include readiness task as a common task
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- lib/common/common.go
+++ lib/common/common.go
@@ -29,6 +29,8 @@ func MakeTask(t *v2alpha2.TaskSpec) (base.Task, error) {
 		return MakeBashTask(t)
 	case LibraryName + "/" + PromoteKubectlTaskName:
 		return MakePromoteKubectlTask(t)
+	case LibraryName + "/" + ReadinessTaskName:
+		return MakeReadinessTask(t)
 	default:
 		return nil, errors.New("Unknown task: " + t.Task)
 	}
